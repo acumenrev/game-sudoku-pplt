682a8343c57d67693e9b3e7bc0c7c0200c377825
acumen.vn@gmail.com <acumen.vn@gmail.com@c295d030-82db-0547-b564-91a38f51c709>
2011-11-10 06:19:04 +0000

    clsTime: tính thời gian trong khi chơi

---
+++ Game_Sudoku/Game_Sudoku/Game_Sudoku/clsTime.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace Game_Sudoku
+{
+    class clsTime
+    {
+        #region Fields
+
+        private Int64 m_milliseconds;
+        private Int64 m_second;
+        private Int64 m_minute;
+        private Int64 m_hour;
+        private static float interval = 1000f;
+
+        #endregion
+
+        #region Properties
+
+        public Int64 Hour
+        {
+            get { return m_hour; }
+            set { m_hour = value; }
+        }
+
+
+        public Int64 Minute
+        {
+            get { return m_minute; }
+            set { m_minute = value; }
+        }
+
+
+        public Int64 Second
+        {
+            get { return m_second; }
+            set { m_second = value; }
+        }
+
+
+        public Int64 Milliseconds
+        {
+            get { return m_milliseconds; }
+            set { m_milliseconds = value; }
+        }
+
+
+        #endregion
+
+        #region Constructors & Methods
+
+        public clsTime()
+        {
+            Second = 0;
+            Minute = 0;
+            Hour = 0;
+            Milliseconds = 0;
+        }
+
+        public void IncreaseTime(GameTime gameTime)
+        {
+            Milliseconds += (long)gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (Milliseconds >= interval)
+            {
+                Second += 1;
+                Milliseconds = 0;
+                if (Second >= 60)
+                {
+                    Minute += 1;
+                    Second = 0;
+                    if (Minute >= 60)
+                    {
+                        Hour += 1;
+                        Minute = 0;
+                    }
+                }
+
+            }
+
+        }
+
+        public string getTime()
+        {
+            return string.Format("{0:00}:{1:00}:{2:00}", this.Hour, this.Minute, this.Second);
+        }
+        #endregion
+
+    }
+}
