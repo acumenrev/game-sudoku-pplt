ac46ef8ecccbd2bb0b6c9d01ac5f575b0154eef3
acumen.vn@gmail.com <acumen.vn@gmail.com@c295d030-82db-0547-b564-91a38f51c709>
2011-11-16 10:19:26 +0000

    Them man hinh LevelScreen --> Hien tai chua xu ly gi het

---
+++ Game_Sudoku/Game_Sudoku/Game_Sudoku/Screens/LevelScreen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameStateManagement;
+using Microsoft.Xna.Framework;
+
+namespace Game_Sudoku.Screens
+{
+    class LevelScreen : MenuScreen
+    {
+        #region Fields
+
+        MenuEntry m_easyMenuEntry;
+        MenuEntry m_mediumMenuEntry;
+        MenuEntry m_hardMenuEntry;
+        MenuEntry m_backMenuEntry;
+
+
+        #endregion
+
+        #region Initialization
+
+        public LevelScreen():base("Choose level")
+        {
+            // Create our menu entries
+            m_backMenuEntry = new MenuEntry("Back");
+            m_easyMenuEntry = new MenuEntry("Easy");
+            m_mediumMenuEntry = new MenuEntry("Medium");
+            m_hardMenuEntry = new MenuEntry("Hard");
+
+
+            // Hook up menu event handlers
+            m_backMenuEntry.Selected += OnCancel;
+            m_easyMenuEntry.Selected += EasyMenuEntrySelected;
+            m_mediumMenuEntry.Selected += MediumMenuEntrySelected;
+            m_hardMenuEntry.Selected += HardMenuEntrySelected;
+
+            // Add entries to the menu
+            MenuEntries.Add(m_easyMenuEntry);
+            MenuEntries.Add(m_mediumMenuEntry);
+            MenuEntries.Add(m_hardMenuEntry);
+            MenuEntries.Add(m_backMenuEntry);
+        }
+        
+
+        #endregion
+
+        #region Handle Input
+
+        void EasyMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+
+        }
+
+        void MediumMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+
+        }
+
+        void HardMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
