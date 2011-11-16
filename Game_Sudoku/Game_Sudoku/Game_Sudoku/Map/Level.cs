4aa2ab05ae3bd6f58301f50795088702c50c3c1d
acumen.vn@gmail.com <acumen.vn@gmail.com@c295d030-82db-0547-b564-91a38f51c709>
2011-11-16 08:18:14 +0000

    Lưu ý: vì m_emptyMatrix trong class này là kiểu string nên khi sử dụng thì phải ép kiểu. Lý do xài kiểu string thay vì int hay float đơn giản vì khi xét EmptyMatrix ở class cho class # thì phải xét xem chỗ nào  = "0" thì mới gán vào . Nếu để int hay float thì cả đám = 0 hết. Nếu muốn sử dụng int hay float thì chỉ cần vào phần constructor của class và gán tất cả các số = 1 số nào đó # 0. Khi xét thì lấy cái nào = 0 thì gán bằng cho phần tử đó

--- Game_Sudoku/Game_Sudoku/Game_Sudoku/Map/Level.cs
+++ Game_Sudoku/Game_Sudoku/Game_Sudoku/Map/Level.cs
@@ -7,6 +7,104 @@ namespace Game_Sudoku.Map
 {
     class Level
     {
+        #region Fields
+
+        private static string[,] m_emptyMatrix = new string[9, 9];
+
+        public string[,] EmptyMatrix
+        {
+            get 
+            { 
+                return m_emptyMatrix;
+            }
+            
+        }
+
+
+        
+        private static Random m_rand = new Random();
+
+        #endregion
+
+        #region Methods & Constructor
+       
+
         public int levelstatus;
+        /// <summary>
+        /// Initialize empty matrix
+        /// </summary>
+        public Level()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    m_emptyMatrix[i, j] = "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Taking out cells from matrix (left empty)
+        /// </summary>
+        /// <param name="level">Game level</param>
+        private static void FillEmpty(int level)
+        {
+            // quantity of empty cells
+            int qCells;
+            switch(level)
+            {
+                // Easy
+                // 40 -> 45 empty cells
+                case 0:
+                    
+                    qCells = m_rand.Next(40, 46);
+                    MakingEmptyCells(qCells);
+                    break;
+                // Medium
+                // 46 -> 49 empty cells
+                case 1:
+                    qCells = m_rand.Next(46, 50);
+                    MakingEmptyCells(qCells);
+                    break;
+                // Hard
+                // 50 -> 54 empty cells
+                case 2:
+                    qCells = m_rand.Next(50, 55);
+                    MakingEmptyCells(qCells);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Fill empty cells with 0
+        /// </summary>
+        /// <param name="quantityOfCells"> Cells need to be left empty</param>
+        private static void MakingEmptyCells(int quantityOfEmptyCells)
+        {
+            for (int i = 0; i < quantityOfEmptyCells; i++)
+            {
+                // check if cells already selected
+                bool isDuplicate = false;
+                do
+                {
+                    int x = m_rand.Next(0, 9);
+                    int y = m_rand.Next(0, 9);
+                    if (m_emptyMatrix[x, y] == "0")
+                    {
+                        isDuplicate = true;
+                    }
+                    else
+                    {
+                        m_emptyMatrix[x, y] = "0";
+                        isDuplicate = false;
+                    }
+                    
+                } while (isDuplicate == true);
+            }
+        }
+
+        #endregion
+
     }
 }
