a689c261c002cb65668d6bfe2d849280afe18a0e
tvthinh91@gmail.com <tvthinh91@gmail.com@c295d030-82db-0547-b564-91a38f51c709>
2011-11-22 17:59:38 +0000

    Change Number finish & Load Map :) successful

--- SodukuWinphone/SodukuWinphone/SodukuWinphone/Screen/GameplayScreen.cs
+++ SodukuWinphone/SodukuWinphone/SodukuWinphone/Screen/GameplayScreen.cs
@@ -18,13 +18,25 @@ namespace SodukuWinphone
         #region Fields
         //Texture2D
         Texture2D m_gameplayBG;
+
         //SpriteFont
         SpriteFont m_gameplayFont;
+
         //SpriteBacth
         SpriteBatch m_spriteBatch;
+
         //Demo String
         int DemoString = 0;
 
+        // Matrix Lock and Result
+        int[,] m_lockMatrixNumber = new int[9, 9];
+        int[,] m_resultMatrixNumber = new int[9, 9];
+
+        //Sudoku
+        Sudoku.Sudoku m_sudoku;
+
+        // Vector Change Number
+        Vector2 m_v2=Vector2.Zero;
         #endregion
         #region Init
         //Contructor
@@ -32,7 +44,19 @@ namespace SodukuWinphone
         {
             TransitionOnTime = TimeSpan.FromSeconds(1.5);
             TransitionOffTime = TimeSpan.FromSeconds(0.5);
+            // Accept what gesture?
             EnabledGestures = GestureType.Tap;
+
+            //Sudoku & Load Map
+            m_sudoku = new Sudoku.Sudoku();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    m_lockMatrixNumber[i, j] = m_sudoku.m_Sudoku[i, j];
+                }
+
+            }
         }
 
         public override void LoadContent()
@@ -64,6 +88,9 @@ namespace SodukuWinphone
             {
                 if (gesture.GestureType == GestureType.Tap)
                 {
+                    m_v2.X = gesture.Position.X;
+                    m_v2.Y = gesture.Position.Y;
+                    ChangeNumber();
                     //DemoString += 2;
                 }
             }
@@ -76,9 +103,77 @@ namespace SodukuWinphone
             m_spriteBatch.Begin();
             m_spriteBatch.Draw(m_gameplayBG, Vector2.Zero, Color.White);
             m_spriteBatch.DrawString(m_gameplayFont, DemoString.ToString(), Vector2.Zero, Color.White);
+            DrawMatrix();
             m_spriteBatch.End();
             base.Draw(gameTime);
         }
+        // Draw Matrix Sudoku
+        public void DrawMatrix()
+        {
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (m_lockMatrixNumber[j, i] != 0)
+                    {
+                        SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+                        string a = m_lockMatrixNumber[j, i].ToString();
+                        float x = (i * 60) + 35;
+                        float y = (j * 50) + 25;
+                        spriteBatch.DrawString(m_gameplayFont, a, new Vector2(x, y), Color.Blue);
+                    }
+
+                    //if (m_lockMatrixNumber[j, i] == 0 &&  m_mapDemo.m_matrixMap[j, i] != 0)
+                    if (m_lockMatrixNumber[j, i] == 0 && m_sudoku.m_Sudoku[j, i] != 0)
+                    {
+                        SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+                        //string a = m_mapDemo.m_matrixMap[j, i].ToString();
+                        string a = m_sudoku.m_Sudoku[j, i].ToString();
+                        float x = (i * 60) + 35;
+                        float y = (j * 50) + 25;
+                        spriteBatch.DrawString(m_gameplayFont, a, new Vector2(x, y), Color.White);
+                    }
+
+
+
+
+                }
+            }
+
+
+        }
+
+        // Tap To Change Number On GameScreen
+        public void ChangeNumber()
+        {
+            float x = (m_v2.X - 15) / 60;
+            float y = (m_v2.Y - 15) / 50;
+                if (x >= 0 && x <= 9)
+                {
+                    if (y >= 0 && y <= 9)
+                    {
+                        if (m_lockMatrixNumber[(int)y, (int)x] == 0)
+                        {
+                            //int numbercurrent = (int)m_mapDemo.m_matrixMap[(int)y, (int)x];
+                            int numbercurrent = m_sudoku.m_Sudoku[(int)y, (int)x];
+                            if (numbercurrent == 9)
+                            {
+                                numbercurrent = 0;
+                            }
+                            //m_mapDemo.m_matrixMap[(int)y, (int)x] = numbercurrent + 1;
+                            m_sudoku.m_Sudoku[(int)y, (int)x] = numbercurrent + 1;
+                          
+                        }
+
+                    }
+
+                }
+            }
+
+
+        }
+
 #endregion
-    }
+    
 }
