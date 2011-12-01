1b831586481ca926e101d5fa4a1cb85c549672ff
tvthinh91@gmail.com <tvthinh91@gmail.com@c295d030-82db-0547-b564-91a38f51c709>
2011-12-01 17:30:47 +0000

    Update

--- SodukuWinphone/SodukuWinphone/SodukuWinphone/Game1.cs
+++ SodukuWinphone/SodukuWinphone/SodukuWinphone/Game1.cs
@@ -39,6 +39,7 @@ namespace SudokuWinphone
             Components.Add(screenManager);
             screenManager.AddScreen(new BackgroundScreen(), null);
             screenManager.AddScreen(new MainMenuScreen(), null);
+            screenManager.AddScreen(new LoginScreen(), null);
         }
 
         /// <summary>
